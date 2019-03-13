8faeb39d28c7d08f1bacb1237b136e5d452122d4
Aaron Powell <me@aaron-powell.com>
2019-03-14 09:50:51 +1100

    adding error handling

--- gobridge/gobridge.go
+++ gobridge/gobridge.go
@@ -3,39 +3,43 @@
 package gobridge
 
 import (
-  "syscall/js"
+	"syscall/js"
 )
 
 var bridgeRoot js.Value
 
 const (
-  bridgeJavaScriptName = "__gobridge__"
+	bridgeJavaScriptName = "__gobridge__"
 )
 
-func registrationWrapper(fn func(this js.Value, args []js.Value) interface{}) func(this js.Value, args []js.Value) interface{} {
-  return func(this js.Value, args []js.Value) interface{} {
-    cb := args[len(args)-1:][0]
+func registrationWrapper(fn func(this js.Value, args []js.Value) (interface{}, error)) func(this js.Value, args []js.Value) interface{} {
+	return func(this js.Value, args []js.Value) interface{} {
+		cb := args[len(args)-1]
 
-    ret := fn(this, args[:len(args)-1])
+		ret, err := fn(this, args[:len(args)-1])
 
-    cb.Invoke(js.Null(), ret)
+		if err != nil {
+			cb.Invoke(err.Error(), js.Null())
+		} else {
+			cb.Invoke(js.Null(), ret)
+		}
 
-    return ret
-  }
+		return ret
+	}
 }
 
 // RegisterCallback registers a Go function to be a callback used in JavaScript
-func RegisterCallback(name string, callback func(this js.Value, args []js.Value) interface{}) {
-  bridgeRoot.Set(name, js.FuncOf(registrationWrapper(callback)))
+func RegisterCallback(name string, callback func(this js.Value, args []js.Value) (interface{}, error)) {
+	bridgeRoot.Set(name, js.FuncOf(registrationWrapper(callback)))
 }
 
 // RegisterValue registers a static value output from Go for access in JavaScript
 func RegisterValue(name string, value js.Value) {
-  bridgeRoot.Set(name, value)
+	bridgeRoot.Set(name, value)
 }
 
 func init() {
-  global := js.Global()
+	global := js.Global()
 
-  bridgeRoot = global.Get(bridgeJavaScriptName)
+	bridgeRoot = global.Get(bridgeJavaScriptName)
 }
